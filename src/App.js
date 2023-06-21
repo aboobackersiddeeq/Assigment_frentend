91d5b9c0bac18bad291da9fc4429d72e71ff26ec
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-06-21 21:13:12 +0530

    Modified file and clean

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import ViewDetails from "./pages/ViewDetails";
+import Error from "./pages/Error";
+import AddDetails from "./pages/AddDetails";
+import { Toaster } from "react-hot-toast";
+import EditDetails from "./pages/EditDetails";
+import { AppContext } from "./context/AppContext";
+import { useState } from "react";
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Provide the usersData and setUsersData to the AppContext */}
+      <AppContext.Provider value={{ usersData, setUsersData }}>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+           {/* Define the routes for different pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-form" element={<Home />} />
+          <Route path="/view-user/:id" element={<ViewDetails />} />
+          <Route path="/add-user" element={<AddDetails />} />
+          <Route path="/edit-user/:id" element={<EditDetails />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </AppContext.Provider>
     </div>
   );
 }
