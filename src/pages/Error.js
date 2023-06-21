2e3013881eba2d592c1d1a5448e5244e205cc668
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-06-22 00:56:56 +0530

    Error page modified

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../styles/error.css'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="not-found-page">
+      <h1 className='error_header'>404 | Page Not Found</h1>
+      <p className='error_footer'>Oops! The page you are looking for does not exist.</p>    
+    </div>
   )
 }
 
